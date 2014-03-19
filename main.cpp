0d13698dbe403865173a8ab15c77f7b0eb1293fe
Alexandre Abita <xouabita@gmail.com>
2014-03-19 17:40:28 +0100

    add code skel

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#ifndef __PROGTEST__
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#endif
+
+bool transform (const char * inFile, const char * outFile) {
+  // To Do
+  return false;
+}
+
+#ifndef __PROGTEST__
+int main (int argc, char * argv []) {
+
+  return 0;
+}
+#endif
